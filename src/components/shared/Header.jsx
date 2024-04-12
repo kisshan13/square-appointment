7a560a18b4eb3ce694e10e11e496d94930f4f005
kisshan13 <kisshan07@outlook.com>
2024-04-12 22:08:49 +0530

    added

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,9 @@
-function Header() {
-    return ( 
-        <header></header>
-     );
+function Header({ isLoggedIn }) {
+    return (
+        <header>
+            <h3 className=" font-medium text-sm">Urban Company</h3>
+        </header>
+    );
 }
 
 export default Header;
