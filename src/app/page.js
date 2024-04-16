3177399938cd3f4cfbae3f1654772f81b7a01a05
lavishabhatia <lavishabhatia1012@gmail.com>
2024-04-16 17:36:09 +0530

    banner men salon

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import PageContainer from "@/components/container/PageContainer";
 import HeroSection from "@/components/landing/HeroSection";
 import ServicesSection from "@/components/landing/ServicesSection";
 import Header from "@/components/shared/Header";
+import Image from "next/image";
+import banner1 from "../../public/banner/banner1.webp";
+import banner2 from "../../public/banner/banner2.webp";
+import MenServices from "@/components/landing/MenServices";
 
 export default function Home() {
   return (
@@ -15,6 +19,9 @@ export default function Home() {
         <PageContainer>
           <HeroSection />
           <ServicesSection />
+          <Image src={banner1} className="w-full rounded-lg" />
+          <MenServices />
+          <Image src={banner2} className=" mt-8 w-full rounded-lg" />
         </PageContainer>
       </main>
     </>
--- src/components/shared/ServiceContainer.jsx
+++ src/components/shared/ServiceContainer.jsx
@@ -1,11 +1,17 @@
 import { Button } from "@radix-ui/themes";
 
-function ServiceContainer({ title, children, button }) {
+function ServiceContainer({ title, children, button, onClick }) {
   return (
     <section className=" my-10">
       <div className="flex items-center justify-between">
         <h3 className=" text-2xl font-medium">{title}</h3>
-        {button ? <Button variant="outline">{button}</Button> : false}
+        {button ? (
+          <Button variant="outline" onClick={onClick}>
+            {button}
+          </Button>
+        ) : (
+          false
+        )}
       </div>
 
       <div className="mt-5">{children}</div>
