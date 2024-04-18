7118886db4b004e3d85ffbf61649392f5c9b8a3f
kisshan13 <kisshan07@outlook.com>
2024-04-18 22:57:15 +0530

    added store api mainCategory

--- src/app/api/store/route.js
+++ src/app/api/store/route.js
@@ -1,6 +1,10 @@
 import square from "@/lib/square";
 import { NextRequest } from "next/server";
 
+BigInt.prototype.toJSON = function () {
+    return this.toString();
+};
+
 const dummyData = {
     hero: {
         heading: "Book your appointments with ease.",
@@ -15,15 +19,41 @@ export async function GET(request) {
     try {
         const categories = await square.catalogApi.listCatalog("", "CATEGORY");
 
+
+        const mainCategoryId = "ULKZ3VBRG3XQD3HAETV2SYHD";
+
+        const mainCategory = [];
+
+        const category = categories.result.objects.filter((c) => {
+
+            if (c.categoryData?.parentCategory?.id === mainCategoryId) {
+                mainCategory.push(c);
+            }
+
+            return !c.categoryData.parentCategory?.id
+        });
+
         const images = await Promise.all(
-            categories.result.objects.map((object) =>
+            category.map((object) =>
                 square.catalogApi.retrieveCatalogObject(
                     object?.categoryData?.imageIds?.at(0)
                 )
             )
         );
 
-        categories.result.objects.forEach((object, index) => {
+        const mainCategoryImages = await Promise.all(
+            mainCategory.map((object) =>
+                square.catalogApi.retrieveCatalogObject(
+                    object?.categoryData?.imageIds?.at(0)
+                )
+            )
+        )
+
+        mainCategory.forEach((object, index) => {
+            object.image = mainCategoryImages[index]?.result?.object?.imageData?.url;
+        })
+
+        category.forEach((object, index) => {
             object.image = images[index]?.result?.object.imageData.url;
         });
 
@@ -32,12 +62,14 @@ export async function GET(request) {
                 hero: {
                     heading: "Home services at your doorstep",
                     categoryText: "What are you looking for ?",
-                    category: categories.result.objects,
+                    category: category,
+                    mainCategory: mainCategory
                 },
             },
             { status: 200 }
         );
     } catch (error) {
+        console.log(error)
         return Response.json({ message: "Internal server error" }, { status: 500 });
     }
 }
