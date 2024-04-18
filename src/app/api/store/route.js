d2efbf6f89f66e9ab3668c607d7b6eeb9c370b4b
kisshan13 <kisshan07@outlook.com>
2024-04-18 21:38:45 +0530

    added apis

--- src/app/api/store/route.js
+++ src/app/api/store/route.js
@@ -1,18 +1,43 @@
+import square from "@/lib/square";
 import { NextRequest } from "next/server";
 
 const dummyData = {
     hero: {
         heading: "Book your appointments with ease.",
-    }
-}
+    },
+};
 
 /**
- * 
- * @param {NextRequest} request 
+ *
+ * @param {NextRequest} request
  */
 export async function GET(request) {
     try {
-        return Response.json(dummyData, { status: 200 });
+        const categories = await square.catalogApi.listCatalog("", "CATEGORY");
+
+        const images = await Promise.all(
+            categories.result.objects.map((object) =>
+                square.catalogApi.retrieveCatalogObject(
+                    object?.categoryData?.imageIds?.at(0)
+                )
+            )
+        );
+
+        categories.result.objects.forEach((object, index) => {
+            object.image = images[index]?.result?.object.imageData.url;
+        });
+
+        return Response.json(
+            {
+                hero: {
+                    heading: "Home services at your doorstep",
+                    categoryText: "What are you looking for ?",
+                    category: categories.result.objects,
+                },
+            },
+            { status: 200 }
+        );
     } catch (error) {
+        return Response.json({ message: "Internal server error" }, { status: 500 });
     }
 }
