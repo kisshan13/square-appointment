e7c4517469d4fcc7fce7de1f21716fe23b2d1877
lavishabhatia <lavishabhatia1012@gmail.com>
2024-04-18 17:07:23 +0530

    routing

--- src/components/landing/ServicesSection.jsx
+++ src/components/landing/ServicesSection.jsx
@@ -1,3 +1,4 @@
+"use client";
 import ServiceCard from "../cards/ServiceCard";
 import ServiceContainer from "../shared/ServiceContainer";
 import Image from "next/image";
@@ -6,6 +7,7 @@ import serv2 from "../../../public/services/serv2.webp";
 import serv3 from "../../../public/services/serv3.webp";
 import serv4 from "../../../public/services/serv4.webp";
 import serv5 from "../../../public/services/serv5.webp";
+import { useRouter } from "next/navigation";
 
 const services = [
   { title: "Waxing", img: serv1 },
@@ -16,17 +18,26 @@ const services = [
 ];
 
 function ServicesSection() {
+  const router = useRouter();
+
   return (
-    <ServiceContainer title={"Salon And Women"} button={"See All"}>
+    <ServiceContainer
+      title={"Salon And Women"}
+      button={"See All"}
+      onClick={() => router.push("/women_salon")}
+    >
       <div className="grid grid-cols-5 gap-5 overflow-x-scroll scrollbar-hide">
         {services?.map((service, i) => (
-          <div className="flex-1 max-w-[250px]">
+          <div
+            key={i}
+            className="flex-1 max-w-[250px]"
+            onClick={() => router.push("/women_salon")}
+          >
             <ServiceCard
               src={service?.img}
               alt={service.title}
               title={service.title}
               href={"#"}
-              key={i}
             />
           </div>
         ))}
