c972cf5985dbf9e1a3fa804926de1174b19ab51c
lavishabhatia <lavishabhatia1012@gmail.com>
2024-04-30 08:06:13 +0530

    changes

--- src/components/cards/ServiceCard.jsx
+++ src/components/cards/ServiceCard.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function ServiceCard({ service, src, alt, title, href }) {
   return (
     <Link href={href}>
-      <div className=" flex flex-col  h-full rounded-lg border border-gray-200 hover:border-gray-400">
-        <div className=" bg-white my-10 px-2">
+      <div className=" flex flex-col h-full rounded-lg border border-gray-200 hover:border-gray-400">
+        <div className="bg-white my-10 px-2">
           <p className="text-base font-medium max-w-full text-black/80">
             {title}
           </p>
